47747de09a89bd8606dcb2ee488cda131f48f0bd
= <=>
2018-03-11 05:44:47 +0000

    add production image upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_one :payment
+  has_many :images
   accepts_nested_attributes_for :payment
 end
